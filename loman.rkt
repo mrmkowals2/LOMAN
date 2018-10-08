0fc25659f9bf9031e24cd4d12ab30f9349d2cfd5
Mike <mkowals2@gmail.com>
2018-10-08 12:07:34 -0400

    Added dice.rkt

--- loman.rkt
+++ loman.rkt
@@ -1,2 +1,18 @@
 #lang racket
 
+; constants
+(define NUM-ROUNDS 1)
+(define RANDOM-WINS 0)
+(define STRATEGY-WINS 0)
+(define TIES 0)
+
+; functions
+(define (d-list n s)
+  (for/list ([i n])
+    (add1 (random s))))
+
+(define (d-list+sum n s)
+  (define l (d-list n s))
+  (define sum (for/sum ([i l]) i))
+  `(,l ,sum))
+
