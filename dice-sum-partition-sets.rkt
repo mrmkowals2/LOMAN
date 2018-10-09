fc7b37ce41dba59ef3aec255a5257eb92d5a2565
Mike <mkowals2@gmail.com>
2018-10-08 20:30:40 -0400

    20181008 - added separate file for dice sum partition sets and more structs/functs

---
+++ dice-sum-partition-sets.rkt
@@ -0,0 +1,90 @@
+#lang racket
+
+(define TWO-PART-SET
+  (list (list 2)))
+
+(define THREE-PART-SET
+  (list (list 3)
+        (list 2 1)))
+
+(define FOUR-PART-SET
+  (list (list 4)
+        (list 3 1)
+        (list 2 2)))
+
+(define FIVE-PART-SET
+  (list (list 5)
+        (list 4 1)
+        (list 3 2)))
+
+(define SIX-PART-SET
+  (list (list 6)
+        (list 5 1)
+        (list 4 2)
+        (list 3 3)
+        (list 3 2 1)))
+
+(define SEVEN-PART-SET
+  (list (list 7)
+        (list 6 1)
+        (list 5 2)
+        (list 4 3)
+        (list 4 2 1)))
+
+(define EIGHT-PART-SET
+  (list (list 8)
+        (list 7 1)
+        (list 6 2)
+        (list 5 3)
+        (list 4 4)
+        (list 5 2 1)
+        (list 4 3 1)))
+
+(define NINE-PART-SET
+  (list (list 9)
+        (list 8 1)
+        (list 7 2)
+        (list 6 3)
+        (list 5 4)
+        (list 6 2 1)
+        (list 5 3 1)
+        (list 4 3 2)))
+
+(define TEN-PART-SET
+  (list (list 10)
+        (list 9 1)
+        (list 8 2)
+        (list 7 3)
+        (list 6 4)
+        (list 5 5)
+        (list 7 2 1)
+        (list 6 3 1)
+        (list 5 4 1)
+        (list 5 3 2)))
+
+(define ELEVEN-PART-SET
+  (list (list 11)
+        (list 9 2)
+        (list 8 3)
+        (list 7 4)
+        (list 6 5)
+        (list 8 2 1)
+        (list 7 3 1)
+        (list 6 4 1)
+        (list 6 3 2)
+        (list 5 4 2)))
+
+(define TWELVE-PART-SET
+  (list (list 12)
+        (list 9 3)
+        (list 8 4)
+        (list 7 5)
+        (list 6 6)
+        (list 9 2 1)
+        (list 8 3 1)
+        (list 7 4 1)
+        (list 6 5 1)
+        (list 7 3 2)
+        (list 6 4 2)
+        (list 5 4 3)))
+
--- loman.rkt
+++ loman.rkt
@@ -1,18 +1,49 @@
 #lang racket
 
-; constants
+
+(require (file "dice-sum-partition-sets.rkt"))
+
+
+; ## constants ##
+
 (define NUM-ROUNDS 1)
 (define RANDOM-WINS 0)
 (define STRATEGY-WINS 0)
 (define TIES 0)
 
-; functions
-(define (d-list n s)
+
+; ## structures ##
+(struct board
+  (one
+   two
+   three
+   four
+   five
+   six
+   seven
+   eight
+   nine))
+
+
+; ## functions ##
+
+; Integer Integer -> List
+; takes a number of dice to be rolled (n) and the number of sides on
+;   each die (s) - returns a list of size n containing the side number
+;   for each die
+; given: 2 6, expect: ([1-6] [1-6])
+; given: 4 5, expect: ([1-5] [1-5] [1-5] [1-5])
+(define/contract (d-list n s)
+  (integer? integer? . -> . list?)
   (for/list ([i n])
     (add1 (random s))))
 
-(define (d-list+sum n s)
-  (define l (d-list n s))
-  (define sum (for/sum ([i l]) i))
-  `(,l ,sum))
+; List -> Integer
+; takes a list of integers and sums the items in the list
+; given: '(1 4), expect: 5
+; given: '(2 5 1), expect: 8
+(define (sum l)
+  (if (empty? l)
+      0
+      (+ (first l) (sum (rest l)))))
 
